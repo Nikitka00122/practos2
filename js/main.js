9cc61039ef3e2deb1c8bfa1fa1937cbd3b78edde
nikita <012nikita_012@mail.ru>
2025-02-11 18:14:19 +0700

    создал 3 столбца

---
+++ js/main.js
@@ -0,0 +1,189 @@
+let eventBus = new Vue();
+
+Vue.component('note', {
+    props: {
+        record_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+        },
+
+        notes: {
+            type: Array,
+            default() {
+                return {}
+            }
+        },
+
+        idColumn: {
+            type: Object,
+            default() {
+                return {}
+            }
+        },
+
+        about:{
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+
+    data() {
+        return {
+            taskTitle: null,
+            task: [],
+        }
+    },
+
+    methods: {
+        // слушатель на то, что добавляется в первую колонку
+        column1Move() {
+            this.$emit('column1_move')
+        },
+
+        // слушатель на кнопку удаления
+        delNote() {
+            this.$emit('del_note')
+        },
+
+        // слушатель на то, что добавляется в первую колонку
+        column2ChangeLeft() {
+            this.$emit('column2_move_left')
+        },
+
+        // слушатель на то, что добавляется в первую колонку
+        column2Move() {
+            this.$emit('column2_move')
+        },
+
+        // создание заявки
+        addTask() {
+            if (this.taskTitle) {
+                this.record_data.tasks.push({
+                    taskTitle: this.taskTitle,
+                    completed: false,
+                });
+                this.taskTitle = null;
+                this.updateCompletedNum();
+                this.save();
+            }
+        },
+
+        // добавление заявок в колонке
+        updateCompletedNum() {
+            let counterCompleted = 0;
+            let counterNotCompleted = 0;
+            for (let el of this.record_data.tasks) {
+                if (el.completed) {
+                    counterCompleted++;
+                } else {
+                    counterNotCompleted++;
+                }
+            }
+            this.record_data.completedNum = (counterCompleted / (counterCompleted + counterNotCompleted)) * 100;
+        },
+
+        checkbox(id) {
+            this.record_data.tasks[id].completed = !this.record_data.tasks[id].completed;
+            this.updateCompletedNum();
+            this.save();
+        },
+
+    },
+
+    template: `
+    <div class="note" >
+            <div class="note_title_block">
+                <h2 class="note_title">{{record_data.note}}</h2>
+                <button @click="delNote()">удалить</button>
+            </div>
+        <div class="tasks">
+                <div v-for="(element, elementId) in record_data.tasks" :key="elementId" class="task">
+                    <div class="set_task">
+                        <h3 class="title_task">{{element.taskTitle}}</h3>
+                        <input @click="checkbox(elementId),column1Move(),column2Move(),column2ChangeLeft()" 
+                               type="checkbox" 
+                               v-model="element.completed" 
+                               :class="{none: record_data.completedNum === 100, disabled: record_data.completedNum > 50 && element.completed && about.lengthColumn1 === 3}" >
+                    </div>
+                    
+                    <div class="date" v-if="record_data.date">
+                        <p>{{record_data.time}}</p>
+                        <p>{{record_data.date}}</p>
+                    </div>
+                </div>
+        </div>
+    </div>
+    `,
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        note: null,
+        recordOne: null,
+        recordTwo: null,
+        recordThree: null,
+        completed: false,
+        about:{
+            signal: false,
+            bufColumn: [],
+            id: null,
+            lengthColumn1: null
+        },
+
+        column1: {
+            notes: [],
+            idColumn: 'СТОЛБЕЦ 1',
+        },
+
+        column2: {
+            notes: [],
+            idColumn: 'СТОЛБЕЦ 2'
+        },
+
+        column3: {
+            notes: [],
+            idColumn: 'СТОЛБЕЦ 3'
+        }
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+        // создание самой заявки, нельзя больше трех заявок создавать в первую колонку
+        creatingRecord() {
+            if (this.note && this.column1.notes.length < 3 && this.recordOne && this.recordTwo && this.recordThree) {
+                this.column1.notes.push({
+                    note: this.note,
+                    tasks: [
+                        {
+                            taskTitle: this.recordOne,
+                            completed: this.completed
+                        },
+                        {
+                            taskTitle: this.recordTwo,
+                            completed: this.completed
+                        },
+                        {
+                            taskTitle: this.recordThree,
+                            completed: this.completed
+                        }
+                    ],
+                    completedNum: 0,
+                });
+                this.note = null;
+                this.recordOne = null;
+                this.recordTwo = null;
+                this.recordThree = null
+                localStorage.todo = JSON.stringify(this.column1.notes);
+            }
+            this.length()
+        },
+    },
+})
